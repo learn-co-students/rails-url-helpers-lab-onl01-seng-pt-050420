ce6b53b89dd0587741a2c2fcbe00490e27e153e1
Catheryn Small <catheryn.small@gmail.com>
2020-09-09 00:38:04 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: 'Catheryne', last_name: 'Small')
+Student.create(first_name: 'Jack', last_name: 'Froste')
+Student.create(first_name: 'Ben', last_name: 'Ten')
+Student.create(first_name: 'Kagome', last_name: 'Higarashi')
+Student.create(first_name: 'Akane', last_name: 'Tendo')
