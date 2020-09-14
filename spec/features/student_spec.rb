283035a101825a581791552f5c00a3afd7fb8cfc
danielkwon89 <danielkwon89@gmail.com>
2020-09-14 09:54:40 -0400

    Done.

--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -54,7 +54,6 @@ end
 describe 'Activate page' do
   before do
     @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
-    binding.pry
   end
 
   it "Should mark an inactive student as active" do
