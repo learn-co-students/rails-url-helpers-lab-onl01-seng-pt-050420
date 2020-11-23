dc26c96570f47b461a796cb787c4d23c25f2412e
isabellagh <isabellagh88@gmail.com>
2020-11-22 22:23:48 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,40 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
   
+  def new
+    @student = Student.new
+  end
+  
+  def create
+    @student = Studennt.new(params[:student])
+    if @student.save
+      flash[:success] = "Studennt successfully created"
+      redirect_to @student
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path
   end
 
   private
 
     def set_student
-      @student = Student.find(params[:id])
+      @student = Student.find_by(params[:id])
     end
 end
